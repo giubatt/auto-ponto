54098dab7a0500d8237a80ecc61960dfb1a8f129
Giuseppe Battistella <giuseppe.battistella@datarisk.io>
2021-01-26 18:33:59 -0300

    Improved help

--- src/index.js
+++ src/index.js
@@ -5,14 +5,21 @@ const meow = createRequire(import.meta.url)('meow');
 
 const cli = meow(`
 	Usage
-	  $ foo <input>
+	  $ auto-ponto --matricula 123
 
 	Options
-	  --rainbow, -r  Include a rainbow
+    --matricula, -m   "Matricula" number
+                      Required
+    
+    --date, -d        Date that is inside the week, format YYYY-MM-DD
+                      (e.g. 2021-02-03 to create timeshee for the week of 2021-02-01 to 2021-02-05)
+                      Defaults to current date
+
+    --output, -o      Name of the file to write the resulting sheet to
+                      Defaults to "outputSheet.xlsx"
 
 	Examples
-	  $ foo unicorns --rainbow
-	  🌈 unicorns 🌈
+	  $ auto-ponto --matricula 123 --date 2021-03-15 --output sheet.xlsx
 `, {
   flags: {
     matricula: {
